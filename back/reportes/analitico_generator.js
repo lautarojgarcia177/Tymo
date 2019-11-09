3af8950830540138da4aaa88e8c13e6feec5ac5f
Lautaro <lautaroxg10@gmail.com>
2019-11-09 11:58:00 -0300

    Avance en generar el pdf analitico

--- back/reportes/analitico_generator.js
+++ back/reportes/analitico_generator.js
@@ -17,27 +17,33 @@ exports.generarPDFAnalitico = function(datos, res, callback) {
         });
 
         doc.addPage({
-            margin: 25
+            margin: 30
+        }).font('Helvetica-Bold');
+
+        //First page header
+
+        doc.image('back/reportes/assets/logo-unc.jpg', 30, 30, {
+            width: 100
         });
+        doc.text('UNIVERSIDAD NACIONAL DE CÓRDOBA', 30, 90,{align: 'left'});
+        doc.text('FACULTAD DE FILOSOFÍA Y HUMANIDADES', 30, 105, {align: 'left'});
 
-        doc.text('UNIVERSIDAD NACIONAL DE CÓRDOBA', {align: 'left'})
-            .moveDown().text('FACULTAD DE FILOSOFÍA Y HUMANIDADES', {align: 'left'});
-
-
-        doc.text('CERTIFICADO ANALÍTICO', {
+        doc.fontSize(15);
+        doc.text('CERTIFICADO ANALÍTICO', 0, 150, {
             align: 'center',
             underline: 'true'
         });
-        doc.moveDown();
-        doc.text('Licenciatura en Historia - PLAN 1994', {
+        doc.text('Licenciatura en Historia - PLAN 1994', 0, 170, {
                 align: 'center'
             });
+
+        doc.text('Titulo: ')
 
         //doc.pipe(fs.createWriteStream('./back/reportes/output.pdf'));
         doc.pipe(res);
 
 // Embed a font, set the font size, and render some text
-        doc.text('Examenes', 100, 100);
+        doc.text('Examenes', 300, 300);
         datos.forEach((examen) => {
            doc.text(JSON.stringify(examen));
         });
